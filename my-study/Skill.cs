63a33d33ee1c37310d4f9d6478545cbacba734e5
licheng <2501093733@qq.com>
2023-12-05 17:58:00 +0800

    增加测试类!

---
+++ my-study/Skill.cs
@@ -0,0 +1,39 @@
+﻿namespace my_study;
+
+[Serializable]
+public class Skill
+{
+    /// 名称 
+    public string name;
+    
+    /// 描述 
+    public string desc;
+    
+    /// 是否延迟技能
+    public bool isDelay;
+    
+    /// 是否蓄力技能
+    public bool isCharge;
+    
+    /// 技能倍率
+    public float rate;
+    
+    /// 技能冷却时间
+    public float cd;
+    
+    
+}
+
+
+
+[Serializable]
+public  class UserA
+{
+    // 名称
+    public string Name;
+    // 年龄
+    public int Age;
+
+}
+
+
