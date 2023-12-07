90bf4ee700ca926e3ba273509aa6dc796c03793b
licheng <2501093733@qq.com>
2023-12-07 11:30:46 +0800

    优化工具!

---
+++ my-study/JsonTest.cs
@@ -0,0 +1,22 @@
+﻿using my_study;
+using Newtonsoft.Json;
+
+public class JsonTest{
+    public static void test(){
+        Console.WriteLine("HelloWorld");
+        var myTest = new MyTest{
+            playerEnum = PlayerEnum.p1,
+            skillEnum = SkillEnum.s技能1
+        };
+        var json = JsonConvert.SerializeObject(myTest);
+        Console.WriteLine(json);
+        var myTest2 = JsonConvert.DeserializeObject<MyTest>(json);
+        Console.WriteLine(myTest2);
+    }
+}
+
+
+public class MyTest{
+    public PlayerEnum playerEnum;
+    public SkillEnum skillEnum;
+}
