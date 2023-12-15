eef92a723907c77b642a7c8ccf0f5c32b3080c27
licheng <2501093733@qq.com>
2023-12-15 11:27:24 +0800

    增加插件测试!

---
+++ my-study/Assets/TestRes.cs
@@ -0,0 +1,11 @@
+﻿namespace my_study.Assets;
+
+public class TestRes
+{
+    public static void Test()
+    {
+        Console.WriteLine("demo");
+        Console.WriteLine("png/demo");
+        Console.WriteLine("png/c-sharp16x16");
+    }
+}
